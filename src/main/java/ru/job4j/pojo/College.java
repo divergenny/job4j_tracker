2873976271afb6fbab78479d01f7efa46182fced
divergenny <divergenny@gmail.com>
2021-04-10 11:09:17 +0500

    2. Модель данных. [#497867]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student denis = new Student();
+        denis.setName("Denis");
+        denis.setSurname("Kim");
+        denis.setPatronymic("Valeryevich");
+        denis.setGroup("304C");
+        denis.setDateOfReceipt("10.04.2021");
+
+        System.out.println(denis.getSurname() + " " + denis.getName() + " " + denis.getPatronymic() + " is studying in group: " + denis.getGroup() + " and date of receipt is: " + denis.getDateOfReceipt() + ".");
+    }
+}
