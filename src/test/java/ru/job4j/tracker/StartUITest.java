a756bbdd96e2d7912e758c432c703878da259b0c
divergenny <divergenny@gmail.com>
2021-04-13 14:50:12 +0500

    5.1. Тестирование. Подготовка данных. [#497806]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 
 public class StartUITest {
@@ -16,5 +17,34 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Chair");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item sofaTest = new Item("Sofa");
+        tracker.add(sofaTest);
+        String[] answers = {
+                String.valueOf(sofaTest.getId()),
+                "Chair"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(sofaTest.getId());
+        assertThat(replaced.getName(), is("Chair"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item sofaTest = new Item("Sofa");
+        tracker.add(sofaTest);
+        Item chairTest = new Item("Chair");
+        tracker.add(chairTest);
+        String[] answers = {
+                String.valueOf(chairTest.getId()),
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(chairTest.getId());
+        assertNull(deleted);
     }
 }
