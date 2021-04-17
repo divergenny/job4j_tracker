b8a64d19e13bc858741d574dd8dbfdc41fb04266
divergenny <divergenny@gmail.com>
2021-04-17 12:57:10 +0500

    0.4. Пользовательские исключения. [#497819]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,16 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (key.equals(value[index])) {
+                rsl = index;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
+    }
+}
