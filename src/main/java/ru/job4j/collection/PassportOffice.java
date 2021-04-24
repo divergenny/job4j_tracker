2534ce38682792fc5f0bb4c4b8bbb1ae7288c083
divergenny <divergenny@gmail.com>
2021-04-24 12:14:40 +0500

    1. Паспорт и жители

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+        Citizen result = null;
+        for (String key : citizens.keySet()) {
+            Citizen citizen = citizens.get(key);
+            if (passport.equals(citizen.getPassport())) {
+                result = citizen;
+            }
+        }
+        return result;
+    }
+}
