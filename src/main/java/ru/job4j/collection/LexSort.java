38d7dbcd646e41950092ded16b34cce39227ebea
divergenny <divergenny@gmail.com>
2021-04-30 11:25:59 +0500

    4. Сортировка номера, fix comments, change logic of comparator.

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -7,23 +7,6 @@ public class LexSort implements Comparator<String> {
     public int compare(String left, String right) {
         String[] leftValues = left.split("\\.");
         String[] rightValues = right.split("\\.");
-        int result = 0;
-        boolean flagEqual = true;
-        int leftValuesLength = leftValues.length;
-        int rightValuesLength = rightValues.length;
-        int itrLength = (Math.min(leftValuesLength, rightValuesLength) - 1);
-        for (int index = 0; index < itrLength; index++) {
-            int leftNum = Integer.parseInt(leftValues[index]);
-            int rightNum = Integer.parseInt(rightValues[index]);
-            result = Integer.compare(leftNum, rightNum);
-            if (result != 0) {
-                flagEqual = false;
-                break;
-            }
-        }
-        if (flagEqual && leftValuesLength != rightValuesLength) {
-            return leftValuesLength > rightValuesLength ? 1 : -1;
-        }
-        return result;
+        return Integer.compare(Integer.parseInt(leftValues[0]), Integer.parseInt(rightValues[0]));
     }
 }
