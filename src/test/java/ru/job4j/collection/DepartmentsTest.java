a993db96466c511a72965106e3b5ab793109c248
divergenny <divergenny@gmail.com>
2021-06-06 14:42:10 +0500

    2. Отсортировать департаменты, add tests for increasing and decreasing cases with full departments name.

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -17,6 +17,41 @@ public class DepartmentsTest {
     }
 
     @Test
+    public void whenFullDepInrOrd() {
+        List<String> input = Arrays.asList("k1/sk2", "k1/sk1/ssk1", "k1/sk1/ssk2", "k1/sk1", "k1",
+                                            "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2", "k1/sk1", "k2");
+        List<String> expect = Arrays.asList("k1",
+                                            "k1/sk2",
+                                            "k1/sk1",
+                                            "k1/sk1/ssk1",
+                                            "k1/sk1/ssk2",
+                                            "k2",
+                                            "k2/sk1",
+                                            "k2/sk1/ssk1",
+                                            "k2/sk1/ssk2");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFullDepDecOrd() {
+        List<String> input = Arrays.asList("k1/sk2", "k1/sk1/ssk1", "k1/sk1/ssk2", "k1/sk1", "k1",
+                "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2", "k1/sk1", "k2");
+        List<String> expect = Arrays.asList("k2",
+                                            "k2/sk1",
+                                            "k2/sk1/ssk1",
+                                            "k2/sk1/ssk2",
+                                            "k1",
+                                            "k1/sk1",
+                                            "k1/sk1/ssk1",
+                                            "k1/sk1/ssk2",
+                                            "k1/sk2");
+        List<String> result = Departments.fillGaps(input);
+        result.sort(new DepDescComp());
+        assertThat(result, is(expect));
+    }
+
+    @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
         List<String> expect = Arrays.asList("k1", "k1/sk1");
