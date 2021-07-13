f62f640af4157f18e6de5f285c0626fff5c32632
Vladislav <divergenny@gmail.com>
2021-07-13 18:41:33 +0500

    1.2. Функциональный интерфейс, remove unnecessary parameter in the toUppersCase method

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -22,7 +22,7 @@ public class FunctionalInterfaces {
         }
         Supplier<List<String>> sup = () -> new ArrayList<String>(map.values());
         Consumer<String> con = s -> System.out.println(s);
-        Function<String, String> func = s -> s.toUpperCase(Locale.ROOT);
+        Function<String, String> func = s -> s.toUpperCase();
         for (String s : sup.get()) {
             con.accept(func.apply(s));
         }
