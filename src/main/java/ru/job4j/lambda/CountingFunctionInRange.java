33460c5b663b4650d8293476ac8bf0ded8eec5f8
Vladislav <divergenny@gmail.com>
2021-07-14 14:40:06 +0500

    5. Подсчет функции в диапазоне.

---
+++ src/main/java/ru/job4j/lambda/CountingFunctionInRange.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class CountingFunctionInRange {
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> rsl = new ArrayList<>();
+        for (; start < end; start++) {
+            rsl.add(func.apply((double) start));
+        }
+        return rsl;
+    }
+}
