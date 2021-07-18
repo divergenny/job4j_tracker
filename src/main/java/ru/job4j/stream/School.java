dd63694a7fa0f9f9f93788e77d16e8ed34bf2a01
Vladislav <divergenny@gmail.com>
2021-07-18 13:56:06 +0500

    1. Фильтрация учеников

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,6 +1,5 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -14,7 +13,8 @@ public class School {
      * @return На выходе аккумулированный результат фильтрации
      * */
     List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> result = students.stream().filter(predict).collect(Collectors.toList());
-        return result;
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
     }
 }
