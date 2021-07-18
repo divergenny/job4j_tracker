5f88165384551f451a9fc8bcaaffc45cf12fbfe8
Vladislav <divergenny@gmail.com>
2021-07-18 13:06:41 +0500

    1. Фильтрация учеников

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,20 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    /**
+     * Cоздаём поток students.stream() и фильтруем .filter(predict)
+     * Сохраняя результат в коллекции .collect(Collectors.toList());
+     * @param students на вход список студентов
+     * @param predict на вход данное сравнение
+     * @return На выходе аккумулированный результат фильтрации
+     * */
+    List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        List<Student> result = students.stream().filter(predict).collect(Collectors.toList());
+        return result;
+    }
+}
